f06d710da3f78c972fca07f16faf0590fd38e8e5
KRISHNAbansal740 <145347360+KRISHNAbansal740@users.noreply.github.com>
2024-09-29 22:11:51 +0530

    Add files via upload

---
+++ DAY2-SUBSCRIBE_.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for _ in range(t):
+    
+    n, x = map(int, input().split())
+
+    subscriptions = (n + 5) // 6  
+
+    total_cost = subscriptions * x
+    
+    print(total_cost)
